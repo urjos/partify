823919b8b4cb93998aa783654de27988e679968a
Josué Torres <josuejesusts@gmail.com>
2026-08-11 15:35:26 -0500

    feat: enhance home screen layout and add user subscription data

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,32 +1,26 @@
+import { HOME_USER } from "@/constants/data";
+import { icons } from "@/constants/icons";
+import images from "@/constants/images";
 import "@/global.css";
-import { Link } from "expo-router";
 import { styled } from "nativewind";
-import { Text } from "react-native";
+import { Image, Text, View } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-7xl text-primary font-sans-extrabold">Home</Text>
-      <Link
-        href="/onboarding"
-        className="mt-4 rounded bg-primary text-white p-4 font-sans-bold"
-      >
-        Go to onboarding
-      </Link>
-      <Link
-        href="/(auth)/sign-in"
-        className="mt-4 rounded bg-primary text-white p-4 font-sans-bold"
-      >
-        Go to Sign In
-      </Link>
-      <Link
-        href="/(auth)/sign-up"
-        className="mt-4 rounded bg-primary text-white p-4 font-sans-bold"
-      >
-        Go to Sign Up
-      </Link>
+      <View className="home-header">
+        <View className="home-user">
+          <Image source={images.avatar} className="home-avatar" />
+          <Text className="home-user-name">{HOME_USER.name}</Text>
+        </View>
+
+        <Image source={icons.add} className="home-add-icon" />
+      </View>
+      <View className="home-balance-card">
+        <Text className="home-balance-label">Balance</Text>
+      </View>
     </SafeAreaView>
   );
 }
